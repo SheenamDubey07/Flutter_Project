db70f01730e6c5bebabcdb17d48463c8f113a4a8
SheenamDubey07 <125239240+SheenamDubey07@users.noreply.github.com>
2023-11-14 22:33:11 +0530

    grocery.dart

---
+++ Flutter Grocery App/lib/data/grocery.dart
@@ -0,0 +1,49 @@
+class GroceryData{
+
+  static List<Map<String,dynamic>> groceryProducts =[
+    {
+      'id' : '1',
+      'name' : 'Bananas',
+      'description' : 'Fresh bananas from Ecuador',
+      'price' : 4.49,
+      'imageUrl' : 'https://fruitboxco.com/cdn/shop/products/asset_2_1024x.jpg?v=1571839043',
+    },
+    {
+      'id' : '2',
+      'name' : 'Organic Apples',
+      'description' : 'Organic Apples grown locally',
+      'price' : 1.65,
+      'imageUrl' : 'https://images.everydayhealth.com/images/apples-101-about-1440x810.jpg',
+    },
+    {
+      'id' : '3',
+      'name' : 'Whole Milk',
+      'description' : 'Fresh, Whole Milk from dairy',
+      'price' : 10.6,
+      'imageUrl' : 'https://www.organicfacts.net/wp-content/uploads/milk-1.jpg',
+    },
+    {
+      'id' : '4',
+      'name' : 'Bread',
+      'description' : 'Farm-Fresh White Bread',
+      'price' : 3.65,
+      'imageUrl' : 'https://assets.bonappetit.com/photos/5c62e4a3e81bbf522a9579ce/4:3/w_3004,h_2253,c_limit/milk-bread.jpg',
+    },
+    {
+      'id' : '5',
+      'name' : 'Grapes',
+      'description' : 'Farm-Fresh Grapes',
+      'price' : 5.05,
+      'imageUrl' : 'https://tse4.mm.bing.net/th?id=OIP.3AFhMFXXqcGTEBVpHhpvpwHaE9&pid=Api&P=0&h=180',
+    },
+    {
+      'id' : '6',
+      'name' : 'Ketchup',
+      'description' : 'Farm-Fresh Red Ketchup',
+      'price' : 20.65,
+      'imageUrl' : 'https://tse4.mm.bing.net/th?id=OIP.EGXR46NbjwQJyiHXZh1x-wHaE8&pid=Api&P=0&h=180',
+    },
+
+
+  ];
+}
